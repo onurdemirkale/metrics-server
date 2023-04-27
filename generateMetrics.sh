546b9d7c0ba9a0284048c744a3abda5eccf37719
Rhonur <onur.demirkale@outlook.com>
2023-04-28 00:30:17 +0300

    feat: add shell script to generate metrics

---
+++ generateMetrics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+METRICS_FILE=metrics_from_special_app.txt
+
+# create the metrics file if it doesn't exist
+if [ ! -f "${METRICS_FILE}" ]; then
+    touch "${METRICS_FILE}"
+fi
+
+# generate dummy metrics and write to file
+for i in {1..10}; do
+    metric_name="metric_${i}"
+    metric_value=$((RANDOM % 1000))
+    echo "${metric_name}=${metric_value}" >> "${METRICS_FILE}"
+done

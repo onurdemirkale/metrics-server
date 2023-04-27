6abd569370ce72d8722df8a2b047c9b204ae9668
Rhonur <onur.demirkale@outlook.com>
2023-04-28 00:34:36 +0300

    feat: implement metrics handler

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -10,10 +13,42 @@ func main() {
 		Addr: ":12345",
 	}
 
+	http.HandleFunc("/metrics", metricsHandler)
+
 	err := httpServer.ListenAndServe()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+
+	// read metric values from file
+	metricsData, err := ioutil.ReadFile("metrics_from_special_app.txt")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// parse metrics data into a map
+	metrics := make(map[string]string)
+
+	for _, line := range bytes.Split(metricsData, []byte("\n")) {
+		if len(line) == 0 {
+			continue
+		}
+		parts := bytes.SplitN(line, []byte("="), 2)
+		if len(parts) != 2 {
+			continue
+		}
+		metrics[string(parts[0])] = string(parts[1])
+	}
+
+	// write metrics to response writer
+	for key, value := range metrics {
+		fmt.Fprintf(w, "%s=%s\n", key, value)
+	}
 }
